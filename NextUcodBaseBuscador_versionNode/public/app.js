8ec2a3cf88a5c63a2ada9a8fa5d8fb9df8bb8c36
Santiago Astudillo <santiman194@gmail.com>
2019-05-08 17:32:57 -0500

    Ver 0.1

--- NextUcodBaseBuscador_versionNode/public/app.js
+++ NextUcodBaseBuscador_versionNode/public/app.js
@@ -1,3 +1,33 @@
+$( document ).ready(function() {
+		$( "#buscar" ).click(function() {
+			cargarCasas();
+		});
+	});
+
+function insertarOfertas(casas) {
+   forEach(casas,function(elemento){
+  		var insertar = "<div class="'card horizontal ofertas'"><div class="'card-image'"><img src="'img/home.jpg'"></div><div class="'card-stacked'"><div class="'card-content'"><div><b>Direccion: " + elemento.Direccion +
+      "</b><p></p></div><div><b>Ciudad: " + elemento.Ciudad +
+      "</b><p></p></div><div><b>Telefono: " + elemento.Telefono +
+      "</b><p></p></div><div><b>Código postal: " + elemento.Codigo_Postal +
+      "</b><p></p></div><div><b>Precio: " + elemento.Tipo +
+      "</b><p></p></div><div><b>Tipo: " + elemento.Precio +
+      "</b><p></p></div></div><div class="'card-action right-align'"><a href="'#'">Ver más</a></div></div></div>";
+  		$(".lista").append(insertar);
+  	});
+  }
+
+function  cargarCasas(){
+	$.ajax({
+  	url: "./data.json",
+  	success: function(datos){
+  		insertarOfertas(datos);
+  	}
+	});
+}
+
+
+
 //Inicializador del elemento Slider
 $("#rangoPrecio").ionRangeSlider({
   type: "double",
